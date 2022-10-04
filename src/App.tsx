09037127f656cd17b2a6c758c9190a11f227da2d
Ulyses Edcel C. Adrales <u.adrales@talkinnovations.com>
2022-10-04 16:19:56 +0800

    Chakra UI Styling

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,15 @@
 import React from 'react';
 import './App.css';
 import { Main } from './components/Main';
+import { ChakraProvider } from '@chakra-ui/react'
 
 function App() {
   return (
+    <ChakraProvider>
     <div className="App">
       <Main />
     </div>
+    </ChakraProvider>
   );
 }
 
--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -1,5 +1,6 @@
+import { Accordion, AccordionButton, AccordionIcon, AccordionItem, AccordionPanel, Box, Button } from "@chakra-ui/react";
 import React, { useEffect, useState } from "react";
-import { Card } from "./Card";
+// import { Card } from "./Card";
 
 export const Main: React.FC<{}> = () => {
     const [count, setCount] = useState<number>(1);
@@ -24,11 +25,47 @@ export const Main: React.FC<{}> = () => {
             {count}
             <button onClick={handleIncrement}>+</button>
             <p>{count % 2 === 0 ? "EVEN" : "ODD"}</p>
+            <Button bg={"orange"} variant={"outline"} isLoading>PRESS ME</Button>
+            <Accordion>
+  <AccordionItem>
+    <h2>
+      <AccordionButton>
+        <Box flex='1' textAlign='left'>
+          Section 1 title
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </AccordionPanel>
+  </AccordionItem>
+
+  <AccordionItem>
+    <h2>
+      <AccordionButton>
+        <Box flex='1' textAlign='left'>
+          Section 2 title
+        </Box>
+        <AccordionIcon />
+      </AccordionButton>
+    </h2>
+    <AccordionPanel pb={4}>
+      Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
+      tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim
+      veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea
+      commodo consequat.
+    </AccordionPanel>
+  </AccordionItem>
+</Accordion>
             {/* <input ref={inputRef} name="description" /> */}
             {/* <ul>
             {this.state.pokemons.map(pokemon => <li key={pokemon.name}>{pokemon.name} - {pokemon.type}</li>)}
             </ul> */}
-            <Card name="Bulbasaur" hp={25} mana={15} group="Leaf" skills={[{name: "Razor Leaf", damage: 5}, {name: "Vine Whip", damage: 7}]} />
+            {/* <Card name="Bulbasaur" hp={25} mana={15} group="Leaf" skills={[{name: "Razor Leaf", damage: 5}, {name: "Vine Whip", damage: 7}]} /> */}
         </main>
     )
 }
